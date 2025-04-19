fce4ac152ff5353583f354bacb3eb7abdaa532d1
Harry Lording <116704220+helording@users.noreply.github.com>
2025-04-19 16:34:04 +0900

    refactor: remove ensure scopes plug

--- lib/shopifex_web/routes.ex
+++ lib/shopifex_web/routes.ex
@@ -36,7 +36,6 @@ defmodule ShopifexWeb.Routes do
 
       pipeline :shopify_session do
         plug(Shopifex.Plug.ShopifySession)
-        plug(Shopifex.Plug.EnsureScopes)
         plug(Shopifex.Plug.LoadInIframe)
       end
 
@@ -62,7 +61,6 @@ defmodule ShopifexWeb.Routes do
         plug(Shopifex.Plug.FetchFlash)
         plug(Shopifex.Plug.LoadInIframe)
         plug(Shopifex.Plug.ShopifyWebhook)
-        plug(Shopifex.Plug.EnsureScopes)
       end
 
       pipeline :shopify_api do
